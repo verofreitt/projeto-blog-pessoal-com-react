fbac06c040a7c3a5101ce19cdc84c79f518bb26e
verofreitt <verofreit@gmail.com>
2024-02-21 18:36:20 -0300

    componentes criados

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import './App.css';
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas/login/Login';
-import Cadastro from './paginas/cadastro/Cadastro';
-import Home from './paginas/home/Home';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 
 function App() {
@@ -21,6 +24,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -15,3 +15,25 @@ export const login = async(url: string, dados: object, setDados: Function) => {
   const resposta = await api.post(url, dados)
   setDados(resposta.data)
 }
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const buscar = async(url: string, setDados: Function, header: Object) => {
+  const resposta = await api.get(url, header)
+  setDados(resposta.data)
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const cadastrar = async(url: string, dados: object, setDados: Function, header: Object) => {
+  const resposta = await api.post(url, dados, header)
+  setDados(resposta.data)
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.put(url, dados, header)
+  setDados(resposta.data)
+}
+
+export const deletar = async(url: string, header: object) => {
+  await api.delete(url, header)
+}
